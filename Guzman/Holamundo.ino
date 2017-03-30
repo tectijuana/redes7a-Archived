55795c1af22b19822b75924949609272f9636952
guzman-garcia <victor.guzman@tectijuana.edu.mx>
2017-03-31 01:14:15 +0200

    Update Holamundo.ino

--- Guzman/Holamundo.ino
+++ Guzman/Holamundo.ino
@@ -1,12 +1,16 @@
-void setup(){
-pinMode(12, OUTPUT); // Pin 12 como Salida.
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
 
-//Ahora le decimos que hacer con esa salida.
-
-void loop(){
-digitalWrite(12, HIGH); // Pone la salida 12 a ON
-delay(1000);            // Espera un segundo
-digitalWrite(12, LOW);  // Pone la salida 12 a OFF
-delay(1000);            // Espera un segundo
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
 }

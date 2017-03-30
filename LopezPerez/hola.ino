e0a928f8a9d95f4165fea93cdd7439f64a8b17c6
lopezW <alumno22106@gmail.com>
2017-03-31 01:11:23 +0200

    Update hola.ino

--- LopezPerez/hola.ino
+++ LopezPerez/hola.ino
@@ -3,7 +3,6 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
   lcd.print("HOLA MUNDO!");
 }
 
@@ -13,4 +12,5 @@ void loop() {
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
+  //propiedad de WILBERT!!!!
 }

dbef2fbf2c04a72ebd5d03a106c3c9f761afeacc
Ernesto Vera Maldonado <activaionuser@gmail.com>
2017-05-25 15:28:42 -0700

    Display hecho en simulador (por falta de arduino)

---
+++ practicas/Vera/Display.ino
@@ -0,0 +1,44 @@
+#include "U8glib.h"
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);  // I2C / TWI
+
+void draw(void) {
+  //Comando gráficos desde este punto
+  u8g.setFont(u8g_font_unifont);
+  //u8g.setFont(u8g_font_osb21);
+  u8g.drawStr( 10, 10, "ITT");
+  u8g.drawStr( 35, 15, "REDS");
+}
+
+void setup(void) {
+  // flip screen, if required
+  // u8g.setRot180();
+ 
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
+
+  // assign default color value
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+}
+
+void loop(void) {
+  // picture loop
+  u8g.firstPage(); 
+  do {
+    draw();
+  } while( u8g.nextPage() );
+ 
+  // Se vuelve a mostrar la imagen con este tiempo
+  delay(120);
+}

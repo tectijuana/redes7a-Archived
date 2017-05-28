49ba96158ed653dbe628335ea3307cdf50a4d8e2
Ernesto Vera Maldonado <activaionuser@gmail.com>
2017-05-28 16:18:23 -0700

    Update conexión wifi.ino

--- practicas/Vera/conexión wifi.ino
+++ practicas/Vera/conexión wifi.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "dd-wrt";
-const char* password = "PS123";
+const char* ssid = "ARRIS-3452";
+const char* password = "";
 
 ESP8266WebServer server(80);
 

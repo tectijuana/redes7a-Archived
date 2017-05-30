93368f7c3386bd97795e01abd5789da68fc781ee
jorge-rosal <jorge.rosal16@tectijuana.edu.mx>
2017-05-30 16:11:56 -0700

    Create HolaMundo.ino

---
+++ practicas/RosalMachain/HolaMundo/HolaMundo.ino
@@ -0,0 +1,12 @@
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);  
